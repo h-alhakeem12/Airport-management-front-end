8b1700d1e86df8f9dfbbc2c718cb14f48b3aa273
Fatii78 <fatii78jj@gmail.com>
2026-04-20 21:10:23 +0300

    StaffSidebar + AdminSidebar

--- src/components/StaffSidebar.jsx
+++ src/components/StaffSidebar.jsx
@@ -1,27 +1,22 @@
 import { Link, useNavigate } from "react-router-dom"
+import "../Sidebar.css"
+import { LuLayoutDashboard, LuClipboardList, LuPlane, LuLogOut } from "react-icons/lu"
 
 const StaffSidebar = () => {
   const navigate = useNavigate()
-
   const handleLogout = () => {
     localStorage.clear()
     navigate("/login")
   }
 
   return (
-    <div>
+    <div className="sidebar-container">
       <h3>Staff Panel</h3>
-
-      <Link to="/StaffDashboard">Dashboard</Link>
-      <br />
-      <Link to="/MyTasks">My Tasks</Link>
-      <br />
-      <Link to="/MyFlights">My Flights</Link>
-      <br /><br />
-
-      <button onClick={handleLogout}>Sign Out</button>
+      <Link className="sidebar-link" to="/StaffDashboard">Dashboard</Link>
+      <Link className="sidebar-link" to="/MyTasks">My Tasks</Link>
+      <Link className="sidebar-link" to="/MyFlights"> My Flights</Link>
+      <button className="logout-button" onClick={handleLogout}> Sign Out</button>
     </div>
   )
 }
-
 export default StaffSidebar
